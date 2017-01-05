11674a9a7404669412547e4d86666bfd71323b1c
Matt D <MDes41@users.noreply.github.com>
2017-01-05 15:36:11 -0700

    Cleaned up routes for google login, starting user info

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 	root to: "home#index"
-	get "/auth/google_oauth2", to: "home#show"
-	get "/auth/google_oauth2/callback", to: "home#index"
+	get "/auth/google_oauth2", as: :google_login
+	get "/auth/google_oauth2/callback", to: "sessions#create"
 end
