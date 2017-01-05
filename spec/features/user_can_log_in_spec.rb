905203beeffbdfe6d69e0183e915a3c97b0f349b
Matt D <MDes41@users.noreply.github.com>
2017-01-05 15:17:38 -0700

    Add google oauth to be able to log in with google

---
+++ spec/features/user_can_log_in_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe "Oauth Login" do 
+	context "user can login" do
+		it "can login" do
+			#as a user I visit the root page for the website
+			#I click login
+			#I am then taken to the signin page for google
+			#I then fill out my credentials
+			#I then click login
+			#And I am taken to my homepage
+		end
+	end
+end
