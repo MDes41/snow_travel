0f9abca7c381870da8f2bda3a271167471b8e4b8
Matt D <MDes41@users.noreply.github.com>
2017-01-07 22:49:37 -0700

    Add forecast model with spec test for json parsing models

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,31 +1,5 @@
 class Forecast
 
-
-	   #  {
-    #   "dt": 1483815600,
-    #   "temp": {
-    #     "day": 259.36,
-    #     "min": 253.56,
-    #     "max": 259.36,
-    #     "night": 257.29,
-    #     "eve": 257.32,
-    #     "morn": 259.36
-    #   },
-    #   "pressure": 738.85,
-    #   "humidity": 39,
-    #   "weather": [
-    #     {
-    #       "id": 600,
-    #       "main": "Snow",
-    #       "description": "light snow",
-    #       "icon": "13d"
-    #     }
-    #   ],
-    #   "speed": 1.66,
-    #   "deg": 244,
-    #   "clouds": 12,
-    #   "snow": 0.12
-    # }
 	attr_reader  :day, :pressure, :humidity, :speed, :wind_dir, :clouds, :snow, :weather, :temp
 
 	def initialize(raw_data)
@@ -34,7 +8,7 @@ class Forecast
 		@temp = create_temps(raw_data[:temp])
 		@pressure = raw_data[:pressure]
 		@humidity = raw_data[:humidity]
-		# @weather = create_weather(raw_data[:weather].first)
+		@weather = create_weather(raw_data[:weather].first)
 		@speed = raw_data[:speed]
 		@wind_dir = raw_data[:deg]
 		@clouds = raw_data[:clouds]
@@ -52,7 +26,7 @@ class Forecast
 		Temp.new(raw_temps)
 	end
 
-	# def create_weather(raw_weather)
-	# 	Weather.new(raw_weather)
-	# end
+	def create_weather(raw_weather)
+		Weather.new(raw_weather)
+	end
 end
