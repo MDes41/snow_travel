2760b979181aa56ed28746698d96092b358bce57
Matt D <MDes41@users.noreply.github.com>
2017-01-09 00:54:43 -0700

    Working with radar add

--- app/controllers/mountains_controller.rb
+++ app/controllers/mountains_controller.rb
@@ -3,6 +3,8 @@ class MountainsController < ApplicationController
 	def show
 		@mountain = Mountain.find(params[:id])
 		@forecasts = Forecast.get_forecasts(@mountain)
+		@satellite_path = WeatherUnderground.get_satalite_path(@mountain, feature: 'satellite')
+		@radar_path = WeatherUnderground.get_radar_path(@mountain, feature: 'radar')
 	end
 
 end
---
+++ app/services/weather_underground.rb
@@ -0,0 +1,57 @@
+class WeatherUnderground
+
+	def initialize(mountain)
+		@api_key = ENV['WU_API_KEY']
+		@request = Faraday.new('http://api.wunderground.com') do |faraday|
+			faraday.request  :url_encoded             # form-encode POST params
+  		faraday.response :logger                  # log requests to STDOUT
+  		faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+  	end
+	end
+	
+	def self.get_webcam_images(mountain, request)
+		new(mountain).weather_underground_response(mountain, request)
+	end
+
+	def self.get_radar_image(mountain, request)
+		new(mountain).weather_underground_response(mountain, request)
+	end
+
+	def self.get_satalite_image(mountain, request)
+		new(mountain).weather_underground_response(mountain, request)
+	end
+
+	def self.get_satalite_path(mountain, request)
+		response = get_satalite_image(mountain, request)
+		"http://api.wunderground.com/" + "#{response.env.url.path}" + "?" + "#{response.env.url.query}"
+	end
+
+		def self.get_radar_path(mountain, request)
+		response = get_radar_image(mountain, request)
+		"http://api.wunderground.com/" + "#{response.env.url.path}" + "?" + "#{response.env.url.query}"
+	end
+
+	def weather_underground_response(mountain, request)
+		url = get_url(mountain, request)
+		params = get_params(request)
+		@request.get do |req|
+			req.url url
+			req.params = params
+		end
+	end
+
+	def get_url(mountain, request)
+		return "/api/#{@api_key}/webcams/lang:EN/q/#{mountain.lat},#{mountain.lon}.json" if request[:feature] == 'webcam'
+		return "/api/#{@api_key}/animatedsatellite/q/#{mountain.lat},#{mountain.lon}.gif" if request[:feature] == 'satellite'
+		return "/api/#{@api_key}/animatedradar/q/#{mountain.lat},#{mountain.lon}.gif" if request[:feature] == 'radar'
+	end
+
+	def get_params(request)
+		if request[:feature] == 'satellite' || request[:feature] == 'radar'
+			{ 'radius' => '300', smooth: 1, 'num' => '8', 'timelabel' => '1', 'timelabel.y' => 15, 'width' => 400, 'height' => 400}
+		else
+			{}
+		end
+	end
+
+end
