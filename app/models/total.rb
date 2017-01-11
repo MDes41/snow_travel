d86fb2f9bfb7510f8f6ac03fb27618fd19ea56e5
Matt D <MDes41@users.noreply.github.com>
2017-01-10 23:43:52 -0700

    Tests for total all passing, need to add mountains states and countrys

--- app/models/total.rb
+++ app/models/total.rb
@@ -31,10 +31,62 @@ class Total
 	end
 
 	def self.snow_days(mtn_forecasts)
-		x = mtn_forecasts.map do |forecast|
-			forecast.weather.id
+		mtn_forecasts.reduce(0) do |sum, forecast|
+			sum + ((600..623).to_a.include?(forecast.weather.id) == true ? 1 : 0)
 		end
-		byebug
-			[600..623].to_a.include?
+	end
+
+	def self.sunny_days(mtn_forecasts)
+		mtn_forecasts.reduce(0) do |sum, forecast|
+			sum + (forecast.weather.id == 800 ? 1 : 0)
+		end
+	end
+
+		def self.temp_high(mtn_forecasts)
+		mtn_forecasts.map do |forecast|
+			forecast.temp.day_temp
+		end.max.round(2)
+	end
+
+	def self.temp_low(mtn_forecasts)
+		mtn_forecasts.map do |forecast|
+			forecast.temp.day_temp
+		end.min.round(2)
+	end
+
+	def self.temp_avg(mtn_forecasts)
+		days = mtn_forecasts.count
+		total_temps = mtn_forecasts.reduce(0) do |sum, forecast|
+			sum + (forecast.temp.day_temp)
+		end
+		(total_temps / days).round(2)
+	end
+
+	def self.wind_high(mtn_forecasts)
+		mtn_forecasts.map do |forecast|
+			forecast.speed
+		end.max.round(2)
+	end
+
+	def self.wind_low(mtn_forecasts)
+		mtn_forecasts.map do |forecast|
+			forecast.speed
+		end.min.round(2)
+	end
+
+	def self.wind_avg(mtn_forecasts)
+		days = mtn_forecasts.count
+		total_wind = mtn_forecasts.reduce(0) do |sum, forecast|
+			sum + (forecast.speed)
+		end
+		(total_wind / days).round(2)
+	end
+
+	def self.wind_dir(mtn_forecasts)
+		dir = Hash.new(0)
+		mtn_forecasts.map do |forecast|
+			dir[forecast.wind_dir] += 1
+		end
+		dir.max_by { |k, v| v }.first
 	end
 end
--- spec/models/total_spec.rb
+++ spec/models/total_spec.rb
@@ -25,7 +25,7 @@ describe 'Total' do
 											        }
 											      ]
 				@raw_data[:deg] = 257
-				3.times do 
+			3.times do 
 				@forecasts << Forecast.new(@raw_data)
 				@raw_data[:snow] += 1
 			end
@@ -98,20 +98,178 @@ describe 'Total' do
 
 	  it 'calculate the snow days icon' do
 	  	@forecasts << Forecast.new(@raw_data)
-	  	@raw_data[:weather] = [{"id" => 800}]
+	  	@raw_data[:weather] = [{"id": 800}]
 	  	@forecasts << Forecast.new(@raw_data)
 	  	@forecasts << Forecast.new(@raw_data)
 	  	@forecasts << Forecast.new(@raw_data)
-	  	@raw_data[:weather] = [{"id" => 615}]
+	  	@raw_data[:weather] = [{"id": 615}]
 	  	@forecasts << Forecast.new(@raw_data)
-	  	@raw_data[:weather] = [{"id" => 600}]
+	  	@raw_data[:weather] = [{"id": 600}]
 	  	@forecasts << Forecast.new(@raw_data)
-	  	@raw_data[:weather] = [{"id" => 620}]
+	  	@raw_data[:weather] = [{"id": 620}]
 	  	@forecasts << Forecast.new(@raw_data)
 
 	  	snow_days = Total.snow_days(@forecasts)
 
 	  	expect(snow_days).to eq(3)
+	  end
+
+	  it 'calculate the sunny days icon' do
+	  	@forecasts << Forecast.new(@raw_data)
+	  	@raw_data[:weather] = [{"id": 615}]
+	  	@forecasts << Forecast.new(@raw_data)
+	  	@raw_data[:weather] = [{"id": 800}]
+	  	@forecasts << Forecast.new(@raw_data)
+	  	@forecasts << Forecast.new(@raw_data)
+
+	  	snow_days = Total.sunny_days(@forecasts)
+
+	  	expect(snow_days).to eq(3)
+	  end
+	end
+
+	context 'Temperature' do
+
+	before(:each) do
+				@forecasts = []
+				@raw_data = {}
+				@raw_data[:snow] = 3
+				@raw_data[:dt] = 1483988400
+				@raw_data[:temp] =  {
+											        "day": 25.00,
+											        "min": 35.15,
+											        "max": 58.06,
+											        "night": 35.15,
+											        "eve": 48.02,
+											        "morn": 58.06
+											      }
+				@raw_data[:weather] = [
+											        {
+											          "id": 800,
+											          "main": "Clear",
+											          "description": "clear sky",
+											          "icon": "01d"
+											        }
+											      ]
+				@raw_data[:deg] = 257
+  end
+
+	  it 'calculates the temp high' do 
+	  	@raw_data[:temp] =  { "day": 25.00 }
+	  	@forecasts << Forecast.new(@raw_data)
+	  	@raw_data[:temp] =  { "day": 55.00 }
+	  	@forecasts << Forecast.new(@raw_data)
+	  	@raw_data[:temp] =  { "day": 35.00 }
+	  	@forecasts << Forecast.new(@raw_data)
+
+	  	temp = Total.temp_high(@forecasts)
+
+	  	expect(temp).to eq(55.00)
+	  end
+
+	   it 'calculates the temp low' do 
+	  	@raw_data[:temp] =  { "day": 55.00 }
+	  	@forecasts << Forecast.new(@raw_data)
+	  	@raw_data[:temp] =  { "day": 25.00 }
+	  	@forecasts << Forecast.new(@raw_data)
+	  	@raw_data[:temp] =  { "day": 35.00 }
+	  	@forecasts << Forecast.new(@raw_data)
+
+	  	temp = Total.temp_low(@forecasts)
+
+	  	expect(temp).to eq(25.00)
+	  end
+
+	  it 'calculates the avg temp' do 
+	  	@raw_data[:temp] =  { "day": 35.00 }
+	  	@forecasts << Forecast.new(@raw_data)
+	  	@raw_data[:temp] =  { "day": 45.00 }
+	  	@forecasts << Forecast.new(@raw_data)
+	  	@raw_data[:temp] =  { "day": 55.00 }
+	  	@forecasts << Forecast.new(@raw_data)
+
+	  	temp = Total.temp_avg(@forecasts)
+
+	  	expect(temp).to eq(45.00)
+	  end
+	end
+
+	context 'Wind' do
+
+	before(:each) do
+				@forecasts = []
+				@raw_data = {}
+				@raw_data[:snow] = 3
+				@raw_data[:dt] = 1483988400
+				@raw_data[:temp] =  {
+											        "day": 58.06,
+											        "min": 35.15,
+											        "max": 58.06,
+											        "night": 35.15,
+											        "eve": 48.02,
+											        "morn": 58.06
+											      }
+				@raw_data[:weather] = [
+											        {
+											          "id": 800,
+											          "main": "Clear",
+											          "description": "clear sky",
+											          "icon": "01d"
+											        }
+											      ]
+				@raw_data[:deg] = 257
+  end
+
+	  it 'calculates the wind high' do 
+	  	@raw_data[:speed] = 10.00
+	  	@forecasts << Forecast.new(@raw_data)
+	  	@raw_data[:speed] = 30.00
+	  	@forecasts << Forecast.new(@raw_data)
+	  	@raw_data[:speed] = 20.00
+	  	@forecasts << Forecast.new(@raw_data)
+
+	  	high = Total.wind_high(@forecasts)
+
+	  	expect(high).to eq(30.00)
+	  end
+
+	  it 'calculates the wind low' do 
+	  	@raw_data[:speed] = 30.00
+	  	@forecasts << Forecast.new(@raw_data)
+	  	@raw_data[:speed] = 10.00
+	  	@forecasts << Forecast.new(@raw_data)
+	  	@raw_data[:speed] = 20.00
+	  	@forecasts << Forecast.new(@raw_data)
+
+	  	low = Total.wind_low(@forecasts)
+
+	  	expect(low).to eq(10.00)
+	  end
+
+	  it 'calculates the wind average' do 
+	  	@raw_data[:speed] = 10.00
+	  	@forecasts << Forecast.new(@raw_data)
+	  	@raw_data[:speed] = 30.00
+	  	@forecasts << Forecast.new(@raw_data)
+	  	@raw_data[:speed] = 20.00
+	  	@forecasts << Forecast.new(@raw_data)
+
+	  	avg = Total.wind_avg(@forecasts)
+
+	  	expect(avg).to eq(20.00)
+	  end
+
+	  it 'calculates the wind dir' do 
+	  	@raw_data[:deg] = 240
+	  	@forecasts << Forecast.new(@raw_data)
+	  	@raw_data[:deg] = 90
+	  	@forecasts << Forecast.new(@raw_data)
+	  	@raw_data[:deg] = 100
+	  	@forecasts << Forecast.new(@raw_data)
+
+	  	dir = Total.wind_dir(@forecasts)
+
+	  	expect(dir).to eq("E")
 	  end
 	end
 end
